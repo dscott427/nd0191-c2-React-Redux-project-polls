e9ba2acc346e4cf50419c922000f167f33d6d7b2
Douglas E Scott <Douglas_E_Scott@carmax.com>
2022-11-05 22:42:34 -0400

    leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -5,15 +5,60 @@ import { useNavigate, Link } from "react-router-dom";
 
 const Leaderboard = (props) => {
 
+    const userKeys = Object.keys(props.users);
 
-return (
-    <div>
-        Leaderboard
-    </div>
-)};
+    const users = userKeys.map(key => {
+        return {
+            fullName: props.users[key].name,
+            userName: key,
+            answers: Object.keys(props.users[key].answers).length,
+            created: Object.keys(props.users[key].questions).length
+        }
+    });
 
-const mapStateToProps = ({ authedUser }) => ({
+    return (
+        <div>
+            <table>
+                <tbody>
+                    <tr key="123">
+                        <th>Name</th>
+                        <th>Answered</th>
+                        <th>Created</th>
+                    </tr>
+                    {users.map((user) => {
+                        return (
+                            <tr key={user.userName}>
+                                <td>
+                                    <div>
+                                        {user.fullName}
+                                    </div>
+                                    <div>
+                                        {user.userName}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>
+                                        {user.answers}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>
+                                        {user.created}
+                                    </div>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+};
+
+const mapStateToProps = ({ authedUser, questions, users }) => ({
     loading: authedUser === null,
-  });
+    questions,
+    users
+});
 
 export default connect(mapStateToProps)(Leaderboard);
